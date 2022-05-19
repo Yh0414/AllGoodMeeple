ee9bb48abf522ab40bd52e80632b570bb598080b
Yahong Pan <yahongp@student.unimelb.edu.au>
2022-05-20 01:58:18 +0800

    separate head+foot as template

--- js/navbar.js
+++ js/navbar.js
@@ -11,6 +11,11 @@ function spreadCategoryList(){
     const category_list = document.getElementById("category_list");
     const chevron_right = document.getElementsByClassName("chevron_right");
 
-    category_list.style.display = "block";
-    chevron_right[0].className = "chevron_right_rotate";
+    if (category_list.style.display == "none") {
+        category_list.style.display = "block";
+        chevron_right[0].classList.add("chevron_right_rotate");
+    } else {
+        category_list.style.display = "none";
+        chevron_right[0].classList.remove("chevron_right_rotate");
+    }
 }

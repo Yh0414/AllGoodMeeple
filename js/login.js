d2106ccb95bed42aa4703f6e10ba41cef64eb68f
Yahong Pan <yahongp@student.unimelb.edu.au>
2022-05-28 11:57:21 +0800

    login + validation

---
+++ js/login.js
@@ -0,0 +1,31 @@
+function validateForm() {
+    let x = document.forms["login_form"]["name"].value;
+    let y= document.forms["login_form"]["email"].value;
+    let z = document.forms["login_form"]["password"].value;
+    let w = document.forms["login_form"]["confirm_password"].value;
+
+    if (x == "") 
+    {
+      alert("Name must be filled out");
+      return false;
+    } else if (y == "") 
+    {
+        alert("Email must be filled out");
+        return false;
+    } else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(y) != true) {
+        alert("You have entered an invalid email address!")
+        return (false)
+    } else if (z == "") 
+    {
+        alert("Password must be filled out");
+        return false;
+    } else if (w == "") 
+    {
+        alert("Confirm Password must be filled out");
+        return false;
+    } else if (w != z) 
+    {
+        alert("Confirm Password is not match with the password");
+        return false;
+    }
+  }

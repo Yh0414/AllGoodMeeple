33d10dbc04489f12c7d91e8c626531a9e1ef50f6
Yahong Pan <yahongp@student.unimelb.edu.au>
2022-06-02 09:05:24 +0800

    filter bar update

---
+++ js/product-list.js
@@ -0,0 +1,9 @@
+var filter_bar = document.getElementById("filter-contain-mobile");
+
+function showFilter() {
+    filter_bar.style.display = "block";
+}
+
+function closeFilter() {
+    filter_bar.style.display = "none";
+}
